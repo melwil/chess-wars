103351b3a3476efd57ef2d94aaacce739036c671
melwil <slettvold@gmail.com>
2014-09-06 20:53:32 +0200

    Added out implementation folder.

---
+++ main/java/no/mesan/sjakk/kursimplementation/AwesomeSjakkmotor.java
@@ -0,0 +1,22 @@
+package no.mesan.sjakk.kursimplementation;
+
+import no.mesan.sjakk.motor.AbstraktSjakkmotor;
+import no.mesan.sjakk.motor.Posisjon;
+
+public class AwesomeSjakkmotor extends AbstraktSjakkmotor {
+
+    @Override
+    protected void finnBesteTrekk(Posisjon posisjon) {
+
+    }
+
+    @Override
+    public String lagetAv() {
+        return null;
+    }
+
+    @Override
+    public String navn() {
+        return null;
+    }
+}
